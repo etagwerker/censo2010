dd4e8bda0f6bf1b47fb831bbe1b051e1b0064916
etagwerker <ernesto@etagwerker.com>
2011-05-19 14:00:02 -0300

    Agregué doc.

--- site.rb
+++ site.rb
@@ -8,6 +8,9 @@ PERSONAS_POR_VIVENDA = calcular_personas_por_vivenda
 
 Cuba.use Rack::Session::Cookie
 
+# Esto define una simple API para ver datos en JSON
+# del Censo de Argentina 2010. 
+# Datos de http://bit.ly/kac3xe (by @jazzido)
 Cuba.define do
   on get do
     on "hello" do
