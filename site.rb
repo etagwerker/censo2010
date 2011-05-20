e525c5c215646e375825e36ce379cde379b481d4
Michel Martens <michel@soveran.com>
2011-05-20 10:33:05 -0300

    Remove trailing whitespaces.

--- site.rb
+++ site.rb
@@ -11,7 +11,7 @@ PERSONAS_POR_VIVENDA = calcular_personas_por_vivenda
 Cuba.use Rack::Session::Cookie
 
 # Esto define una simple API para ver datos en JSON
-# del Censo de Argentina 2010. 
+# del Censo de Argentina 2010.
 # Datos de http://bit.ly/kac3xe (by @jazzido)
 Cuba.define do
   on get do
@@ -19,11 +19,11 @@ Cuba.define do
       template = Tilt::HamlTemplate.new('templates/hello.html.haml')
       res.write template.render
     end
-    
-    on "raw_data" do 
+
+    on "raw_data" do
       res.write RAW_DATA.to_json
     end
-    
+
     on "personas_por_vivienda" do
       res.write PERSONAS_POR_VIVENDA.to_json
     end
