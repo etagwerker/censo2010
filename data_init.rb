c023d8cee82c73eb2621aaee273bbd36630665bf
etagwerker <ernesto@etagwerker.com>
2011-06-05 20:19:24 -0300

    Agregué métodos para cargar la BD, saqué carga a memoria.

--- data_init.rb
+++ data_init.rb
@@ -1,34 +1,31 @@
+require "ruby-debug"
+
 # carga datos de un csv en un array de hashes
+# y en la base de datos
 def load_raw_data
+  puts "Loading raw data"
+  
   result = []
   column_names = nil
+  pos = 0
   CSV.foreach("datos_preliminares_censo_2010_argentina.csv", :col_sep =>',', :row_sep =>:auto) do |row|
+    pos += 1
     unless column_names
       column_names = []
       column_names = row
     else
+      puts "Loading departamento ##{pos}"
       hash = {}
       row.each_with_index do |value, pos|
         hash[column_names[pos]] = value
       end
       result << hash
+      unless Departamento.first(:conditions => {:nombre => hash["DEPARTAMENTO"], :provincia => hash["PROVINCIA"]})
+        Departamento.new(hash).save  
+      end
+      puts "Loaded departamento"
     end
   end
-  result
+  puts "Loaded raw data"
 end
 
-# usa RAW_DATA
-def calcular_personas_por_vivenda
-  result = []
-  RAW_DATA.each do |source|
-    hash = {}
-    hash["DEPARTAMTO"] = source["DEPARTAMTO"]
-    hash["TOTAL_PERSONAS"] = source["TOTAL_MUJERES"].to_i + source["TOTAL_VARONES"].to_i
-    hash["TOTAL_VIVIENDAS"] = source["TOTAL_VIVIENDAS"].to_i
-    hash["PERSONAS_POR_VIVIENDA"] = hash["TOTAL_PERSONAS"].to_f / hash["TOTAL_VIVIENDAS"]
-    hash["VARONES_POR_VIVIENDA"] = source["TOTAL_VARONES"].to_f / hash["TOTAL_VIVIENDAS"]
-    hash["MUJERES_POR_VIVIENDA"] = source["TOTAL_MUJERES"].to_f / hash["TOTAL_VIVIENDAS"]
-    result << hash
-  end
-  result
-end
