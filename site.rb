2fe147572d04784b76e36963c07fc37f2e66fa1a
etagwerker <ernesto@etagwerker.com>
2011-06-05 20:17:58 -0300

    Cambié la implementación y agregué métodos útiles.
    Ya no usa constantes en memoria, ahora usa Datamapper para acceder a una BD Posgres.

--- site.rb
+++ site.rb
@@ -2,12 +2,24 @@
 
 require "cuba"
 require "csv"
-require "json"
 require "haml"
+require "data_mapper"
+require "dm-postgres-adapter"
+require "dm-migrations"
+require "dm-serializer/to_json"
+
 require "./data_init"
 
-RAW_DATA = load_raw_data
-PERSONAS_POR_VIVENDA = calcular_personas_por_vivenda
+VERSION = "0.2"
+
+Dir.glob(File.dirname(__FILE__) + '/app/models/*') {|file| require file}
+DataMapper.finalize
+
+DataMapper::Logger.new($stdout, :debug)
+DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://mnwo:mnw0@localhost/censo_2010')
+DataMapper.auto_upgrade!
+
+load_raw_data
 
 Cuba.use Rack::Session::Cookie
 
@@ -17,6 +29,13 @@ Cuba.use Rack::Session::Cookie
 Cuba.define do
   def partial(view, options = {})
     render("views/#{view}.haml", options)
+  end
+  
+  # takes buenos_aires and it turns it into
+  # BUENOS AIRES, for example
+  def sanitize(param)
+    result = param.sub(/_/,' ')
+    result.upcase!
   end
 
   def layout(layout, options)
@@ -28,16 +47,67 @@ Cuba.define do
   end
 
   on get do
+    
+    on "version" do |variable|
+      res.write VERSION.to_json
+    end
+    
     on "" do
       page("home")
     end
+    
+    on "cabeceras" do 
+      res.write Departamento.all(:fields => [:cabecera], :unique => true, :order => :cabecera.asc).map(&:cabecera).to_json
+    end
+        
+    on "departamentos" do
+      res.write Departamento.all(:fields => [:nombre], :unique => true, :order => :nombre.asc).map(&:nombre).to_json        
+    end
+    
+    on "provincias" do 
+      res.write Departamento.all(:fields => [:provincia], :unique => true, :order => :provincia.asc).map(&:provincia).to_json
+    end
 
     on "raw_data" do
-      res.write RAW_DATA.to_json
+      # points todavia no lo muestra porque no se 
+      # como hace datamapper para cargar las associations
+      # 'eagerly', cuando lo sepa sera arreglado (@etagwerker)
+      res.write Departamento.all.to_json
     end
 
     on "personas_por_vivienda" do
-      res.write PERSONAS_POR_VIVENDA.to_json
+      res.write Departamento.all.to_json(:only => [:nombre, :cabecera, :provincia], :methods => [:total_personas, :personas_por_vivienda, :mujeres_por_vivienda, :varones_por_vivienda])
     end
+    
+    on ":provincia" do |pcia|
+      on "" do
+        res.write Departamento.all(:provincia => sanitize(pcia)).to_json        
+      end
+      
+      on "departamentos" do
+        res.write Departamento.all(:provincia => sanitize(pcia), :order => :nombre.asc).map(&:nombre).to_json        
+      end
+      
+      on "cabeceras" do 
+        res.write Departamento.all(:provincia => sanitize(pcia), :order => :cabecera.asc).map(&:cabecera).to_json
+      end
+      
+      on "personas_por_vivienda" do
+        res.write Departamento.all(:provincia => sanitize(pcia)).to_json(:only => [:nombre, :cabecera, :provincia], :methods => [:total_personas, :personas_por_vivienda, :mujeres_por_vivienda, :varones_por_vivienda])
+      end
+      
+      on ":departamento" do |depto|
+        
+        on "" do 
+          res.write Departamento.all(:nombre => sanitize(depto), :provincia => sanitize(pcia)).to_json            
+        end
+        
+        on "personas_por_vivienda" do
+          res.write Departamento.all(:nombre => sanitize(depto), :provincia => sanitize(pcia)).to_json(:only => [:nombre, :cabecera, :provincia], :methods => [:total_personas, :personas_por_vivienda, :mujeres_por_vivienda, :varones_por_vivienda])
+        end
+      end
+          
+    end
+      
   end
 end
