e180190f7f16ff87b283ffe6386f82cccfefadfb
etagwerker <ernesto@etagwerker.com>
2011-06-08 17:02:17 -0300

    Cambié el nombre de los links.

--- test/home.rb
+++ test/home.rb
@@ -7,13 +7,13 @@ scope do
 
     assert has_content?("Esta es la API del Censo Argentino 2010")
 
-    click_link "Raw Data"
+    click_link "GET /raw_data"
 
-    assert has_content?('{"DEPARTAMTO":"SAN NICOLAS"')
+    assert has_content?('{"DEPARTAMENTO":"SAN NICOLAS"')
 
     visit "/"
 
-    click_link "Personas por Vivienda"
+    click_link "GET /personas_por_vivienda"
 
     assert has_content?('"PERSONAS_POR_VIVIENDA":2.929014763482977')
   end
